8770b832900e9a9691f6620aafb67c0cccaa881c
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-11-23 22:27:01 +0100

    scrollTo menu

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,17 @@ const traitVertical = $('.trait.vertical');
 const traitHorizontalBottom = $('.trait.horizontal-bottom');
 
 $(function(){
+    // Animation traits
     TweenLite.to(traitHorizontalTop, 1, {scaleX: 1, delay: 3});
     TweenLite.to(traitVertical, 4, {scaleY: 1, delay: 4});
     TweenLite.to(traitHorizontalBottom, 1, {scaleX: 1, delay: 8});
+
+    // Animation scrollTo
+    $('.menu-header a').on('click', function(e){
+        e.preventDefault();
+        var section = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(section).offset().top
+        }, 500);
+    });
 });
